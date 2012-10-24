fcc1bb6561cb77e80caa71df4f48a13c98656a30
Ildar Karimov <thevery@yandex-team.ru>
2012-10-24 10:19:59 +0400

    first draft

---
+++ Amazon/src/com/yandex/sample/droidcon/amazon/MyActivity.java
@@ -0,0 +1,23 @@
+package com.yandex.sample.droidcon.amazon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
