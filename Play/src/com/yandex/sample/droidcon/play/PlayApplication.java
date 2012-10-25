dc444a4078c40fb5f7d4d61d6049fc3b2e431c99
Ildar Karimov <thevery@yandex-team.ru>
2012-10-25 19:03:33 +0100

    initial play version

---
+++ Play/src/com/yandex/sample/droidcon/play/PlayApplication.java
@@ -0,0 +1,29 @@
+package com.yandex.sample.droidcon.play;
+
+import com.yandex.sample.droidcon.library.LibraryApplication;
+
+public class PlayApplication extends LibraryApplication {
+    @Override
+    public int getSubscriptionDaysLeft() {
+        return EXPIRED;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public boolean getEntitlementState(String sku) {
+        return false;
+    }
+
+    @Override
+    public int getConsumableCount(String sku) {
+        return 0;
+    }
+
+    @Override
+    public void purchase(String sku) {
+    }
+}
